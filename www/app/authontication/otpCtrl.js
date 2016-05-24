c3ba7f68ec8a5e843437d8fc6ae312c9b80f0329
sachin22in <node@Cognizants-mac.local>
2016-05-24 19:46:42 +0530

    top api

--- www/app/authontication/otpCtrl.js
+++ www/app/authontication/otpCtrl.js
@@ -6,6 +6,27 @@
 
     function otpCtrl($scope,$state,$rootScope,$ionicHistory,$http, $location, ionicDatePicker, httpService) {  
      
+     $scope.sendOTP = function(){
+ 		var URL = 'http://login.smsgatewayhub.com/RestAPI/MT.svc/mt';
+ 		var data = {"Account":{"User":"Glassmates","Password":"fiesta86","SenderId":"WEBSMS","Channel":"2","DCS":"0","SchedTi me":null,"GroupId":null},"Messages":[{"Number":"919558515827","Text":"Hello test message % ignore123 %"}]};
+
+ 		$http({
+	        method: 'POST',
+	        url: URL,
+	        headers: {
+			   'Content-Type': undefined
+			},
+	        crossDomain: true,
+	        data: data
+	    })
+	    .then(function(response){
+            
+        }, 
+        function(error){
+            console.log(error);
+        });
+
+     }
   
     }
 
